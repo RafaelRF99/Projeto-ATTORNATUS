8af84b7dc3c781a8e4155eaad2e38fb22fe7189f
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-12-01 15:39:34 -0300

    feat: add loading + erro usuario

--- src/app/components/display-user/display-user.component.ts
+++ src/app/components/display-user/display-user.component.ts
@@ -19,13 +19,24 @@ export class DisplayUserComponent implements OnInit {
   };
   displayedColumns: string[] = ['icon', 'name', 'email', 'action'];
   dataSource!: IUsers[];
+  error: boolean = false;
+  loading: boolean = true;
 
   constructor(public dialog: MatDialog, private service: UsersService) {}
 
   ngOnInit(): void {
-    this.service.getAllUsers().subscribe((dado) => {
-      this.dataSource = dado;
-    });
+    this.service.getAllUsers().subscribe(
+      (data) => {
+        this.dataSource = data;
+        this.error = false;
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Erro na requisição:', error);
+        this.loading = false;
+        this.error = true;
+      }
+    );
   }
 
   openDialog(user: IUsers) {

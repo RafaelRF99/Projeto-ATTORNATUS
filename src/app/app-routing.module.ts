5994e813d0312c9fe7370f10fd83a462fe490196
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-12-01 16:57:37 -0300

    chore: removido rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { UpdateUserComponent } from './components/update-user/update-user.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  },
-  {
-    path: 'create/:id',
-    component: UpdateUserComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
